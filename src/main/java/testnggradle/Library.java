8fd322c8e8cd34d808a919b0cf09305462f1ed8c
Mohammad Tufail Ahmed <tufailahmedkhan@gmail.com>
2024-01-04 13:44:56 +0530

    added nithya changes

--- src/main/java/testnggradle/Library.java
+++ src/main/java/testnggradle/Library.java
@@ -9,6 +9,6 @@ public class Library {
     }   
 
     public String getMessage() {
-        return "Hello , how are you today , Thanks HHHHHello!!!";
+        return "Hello , how are you today , Thanks HHHHLLLLHello!!!";
     }
 }
